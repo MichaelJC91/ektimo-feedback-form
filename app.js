959eb79787daf22e406537b579245f821d7614be
Michael Carniato <mcarniato1991@gmail.com>
2016-10-31 05:27:54 +0000

    add index.js. Refactor app.js code

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require("express"),
     app     = express(),
     bodyParser = require("body-parser"),
     mongoose    = require("mongoose"),
-    Feedback    = require("./models/feedback"),
-    moment      = require("moment-timezone"),
-    date        = moment().tz("Australia/Melbourne").format("Do MMM, YYYY");
+    moment      = require("moment-timezone");
     
+//Routes
+var indexRoute = require("./routes/index")
 
 //Set View Engine Template
 app.set("view engine", "ejs");
@@ -15,60 +15,14 @@ app.use(express.static(__dirname + "/public"));
 
 //Mongoose Directory
 mongoose.connect(process.env.DATABASEURL);
-// mongoose.connect("mongodb://michael:comics1991@ds139277.mlab.com:39277/ektimo_feedback_form");
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 //Use moment.js library across all files
 app.locals.moment = moment;
 
-//Get Feedback Form
-app.get("/", function(req, res){
-    res.render("feedback");
-});
-
-//Create New Feedback
-app.post("/", function(req, res){
-    //Get data from form and add to campgrounds array
-    var companyName = req.body.companyName,
-        rating      = req.body.rating,
-        name        = req.body.name,
-        reason      = req.body.reason,
-        post        = req.body.postToWebsite,
-        chooseName  = req.body.chooseName;
-    //Create new feedback form with above variables ^^
-    var newFeedbackForm = {
-        
-        companyName: companyName,
-        rating: rating,
-        name: name,
-        reason: reason,
-        postToWebsite: post,
-        websiteAppear: chooseName,
-        date: date
-        
-    };
-    
-    //Save Feedback form to DB
-    Feedback.create(newFeedbackForm, function(err){
-        if(err) {
-            console.log(err);
-        }   else {
-            res.redirect("/feedback-data");
-        }
-    });
-});
-
-//Get Feedback JSON Data
-app.get("/feedback-data", function(req, res){
-    Feedback.find({}, function(err, foundFeedback){
-        if(err) {
-            console.log(err);
-        } else {
-            res.json({feedback: foundFeedback});
-        }
-    });
-});
+//Route of index page
+app.use(indexRoute);
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("App Has Started!");
---
+++ routes/index.js
@@ -0,0 +1,56 @@
+var express     = require("express"),
+    router      = express.Router(),
+    Feedback    = require("../models/feedback"),
+    moment      = require("moment-timezone"),
+    date        = moment().tz("Australia/Melbourne").format("Do MMM, YYYY");
+    
+//Root Route (Feedback Form)
+router.get("/", function(req, res){
+    res.render("feedback");
+});
+
+//Create New Feedback
+router.post("/", function(req, res){
+    //Get data from form and add to campgrounds array
+    var companyName = req.body.companyName,
+        rating      = req.body.rating,
+        name        = req.body.name,
+        reason      = req.body.reason,
+        post        = req.body.postToWebsite,
+        chooseName  = req.body.chooseName;
+        
+    //Create new feedback form with above variables ^^
+    var newFeedbackForm = {
+        
+        companyName: companyName,
+        rating: rating,
+        name: name,
+        reason: reason,
+        postToWebsite: post,
+        websiteAppear: chooseName,
+        date: date
+        
+    };
+    
+    //Save Feedback form to DB
+    Feedback.create(newFeedbackForm, function(err){
+        if(err) {
+            console.log(err);
+        }   else {
+            res.redirect("http://ektimo.com.au/home/");
+        }
+    });
+});
+
+//Get Feedback JSON Data
+router.get("/feedback-data", function(req, res){
+    Feedback.find({}, function(err, foundFeedback){
+        if(err) {
+            console.log(err);
+        } else {
+            res.json({feedback: foundFeedback});
+        }
+    });
+});
+
+module.exports = router;
